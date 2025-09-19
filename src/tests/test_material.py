f92b47944745453a367fb805329d2fc8ecb1ab4f
Scott Sibole <scott.sibole@gmail.com>
2025-09-19 11:32:26 -0700

    test uncoupled materials

--- src/tests/test_material.py
+++ src/tests/test_material.py
@@ -22,3 +22,24 @@ def test_unconstrained_material(hex8_febmesh, tmp_path):
         my_model.save(model_file)
         result = feb.model.run_model(model_file)
         assert result.returncode == 0
+
+
+def test_uncoupled_material(hex8_febmesh, tmp_path):
+    for material_cls in get_args(feb.material.UncoupledMaterials):
+        my_model = feb.model.Model(mesh=hex8_febmesh)
+        for i, element in enumerate(my_model.mesh.elements):
+            my_mat = material_cls(name=element.name, id=i + 1)
+            if hasattr(my_mat, "mat_axis"):
+                my_mat.mat_axis = feb.material.MaterialAxisVector()
+            elif hasattr(my_mat, "fiber"):
+                my_mat.fiber = feb.material.FiberVector()
+            my_model.material.add_material(my_mat)
+            my_model.mesh_domains.add_solid_domain(
+                feb.meshdomains.SolidDomain(name=element.name, mat=element.name)
+            )
+        model_file = tmp_path.joinpath(
+            f"{my_model.material.all_materials[0].type.replace(' ', '_')}.feb"
+        )
+        my_model.save(model_file)
+        result = feb.model.run_model(model_file, silent=True)
+        assert result.returncode == 0
