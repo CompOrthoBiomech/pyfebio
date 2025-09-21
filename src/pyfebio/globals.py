23162e74d7316b5294723af5da6203d4b692431a
Scott Sibole <scott.sibole@gmail.com>
2025-09-20 18:13:59 -0700

    changed default Temperature

--- src/pyfebio/globals.py
+++ src/pyfebio/globals.py
@@ -2,7 +2,7 @@ from pydantic_xml import BaseXmlModel, element
 
 
 class Constants(BaseXmlModel, validate_assignment=True):
-    T: float = element(default=0)
+    T: float = element(default=298)
     P: float = element(default=0)
     R: float = element(default=8.31446)
     Fc: float = element(default=96485.3)
