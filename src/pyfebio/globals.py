bd55c7f171f4cbeb3f613994627dd6e33e7a5574
Scott Sibole <scott.sibole@gmail.com>
2025-09-22 11:28:44 -0700

    fixed constant values

--- src/pyfebio/globals.py
+++ src/pyfebio/globals.py
@@ -4,8 +4,8 @@ from pydantic_xml import BaseXmlModel, element
 class Constants(BaseXmlModel, validate_assignment=True):
     T: float = element(default=298)
     P: float = element(default=0)
-    R: float = element(default=8.31446)
-    Fc: float = element(default=96485.3)
+    R: float = element(default=8.314e-6)
+    Fc: float = element(default=96485e-9)
 
 
 class Globals(BaseXmlModel, validate_assignment=True):
